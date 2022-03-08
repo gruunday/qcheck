0b51e167c17cf2f4acba79940eb7f01ce6011b4e
Tom Doyle <tom.doyle@dcu.ie>
2022-03-08 17:12:53 +0000

    Change how qcheck main is called and add run function

--- src/qcheck/main.py
+++ src/qcheck/main.py
@@ -39,64 +39,67 @@ def colour(percent):
     else:
         return 32 # green
 
-if len(sys.argv) == 2:
-    username = sys.argv[1]
-else:
-    username = ""
 
-# run quota, read in the output.
-cmd = f"/usr/bin/quota -p -w -Q {username}" 
-quota = get_quota(cmd, 5)
-quota = quota.decode("utf-8")
-
-# split the output into a list.
-quota = quota.split("\n")
-
-labels = quota[1].split()
-values = quota[2].split()
-blocks = [ int(''.join(i for i in values[x] if i.isdigit())) for x in range(1,4)]
-files = [ int(''.join(i for i in values[x] if i.isdigit())) for x in range(5,8)]
-#blocks = [int(values[1]), int(values[2]), int(values[3]), int(values[4])]
-#files = [int(values[5]), int(values[6]), int(values[7]), int(values[8])]
-
-# if you change the bar width, you'll also need to change the value in the
-# format string below.
-bar_width = 35.0
-
-# print the header
-if username == "":
-    account_owner = "your"
-else:
-    account_owner = "%s's" % username
-
-sys.stdout.write("\nStorage space report for %s \033[1;34mSoC\033[0m account:\n" % account_owner)
-
-
-if blocks:
-    blocks_percent = (blocks[0] * 1.0 / blocks[1] * 1.0) * 100
-    blocks_bar_width = blocks_percent * (bar_width / 100)
-
-    if blocks_bar_width > bar_width:
-        blocks_bar_width = bar_width
-
-    # print the blocks quota
-    format = "%8s: |\033[1;%dm%-35s\033[0m|\033[0;33m%3d\033[0m%%, \033[0;33m%4d\033[0mMB used, \033[0;33m%4d\033[0mMB free\n"
-    sys.stdout.write(format % ("     storage", colour(blocks_percent), "=" * int(blocks_bar_width), blocks_percent, blocks[0] / 1024, (blocks[1] - blocks[0]) / 1024))
-
-if files:
-    if files[1] == 0:
-       files_percent = 0
+def run():
+    if len(sys.argv) == 2:
+        username = sys.argv[1]
     else:
-      files_percent = (files[0] * 1.0 / files[1] * 1.0) * 100
-    files_bar_width = files_percent * (bar_width / 100)
+        username = ""
+    
+    # run quota, read in the output.
+    cmd = f"/usr/bin/quota -p -w -Q {username}" 
+    quota = get_quota(cmd, 5)
+    quota = quota.decode("utf-8")
+    
+    # split the output into a list.
+    quota = quota.split("\n")
+    
+    labels = quota[1].split()
+    values = quota[2].split()
+    blocks = [ int(''.join(i for i in values[x] if i.isdigit())) for x in range(1,4)]
+    files = [ int(''.join(i for i in values[x] if i.isdigit())) for x in range(5,8)]
+    #blocks = [int(values[1]), int(values[2]), int(values[3]), int(values[4])]
+    #files = [int(values[5]), int(values[6]), int(values[7]), int(values[8])]
+    
+    # if you change the bar width, you'll also need to change the value in the
+    # format string below.
+    bar_width = 35.0
+    
+    # print the header
+    if username == "":
+        account_owner = "your"
+    else:
+        account_owner = "%s's" % username
+    
+    sys.stdout.write("\nStorage space report for %s \033[1;34mSoC\033[0m account:\n" % account_owner)
+    
+    
+    if blocks:
+        blocks_percent = (blocks[0] * 1.0 / blocks[1] * 1.0) * 100
+        blocks_bar_width = blocks_percent * (bar_width / 100)
+    
+        if blocks_bar_width > bar_width:
+            blocks_bar_width = bar_width
+    
+        # print the blocks quota
+        format = "%8s: |\033[1;%dm%-35s\033[0m|\033[0;33m%3d\033[0m%%, \033[0;33m%4d\033[0mMB used, \033[0;33m%4d\033[0mMB free\n"
+        sys.stdout.write(format % ("     storage", colour(blocks_percent), "=" * int(blocks_bar_width), blocks_percent, blocks[0] / 1024, (blocks[1] - blocks[0]) / 1024))
+    
+    if files:
+        if files[1] == 0:
+           files_percent = 0
+        else:
+          files_percent = (files[0] * 1.0 / files[1] * 1.0) * 100
+        files_bar_width = files_percent * (bar_width / 100)
+    
+        if files_bar_width > bar_width:
+            files_bar_width = bar_width
+    
+        # print the files quota
+        format = "%8s: |\033[1;%dm%-35s\033[0m|\033[0;33m%3d\033[0m%%, \033[0;33m%4d\033[0mUsed,    \033[0;33m%4d\033[0mFree\n"
+        sys.stdout.write(format % ("  file count", colour(files_percent), "=" * int(files_bar_width), files_percent, files[0], (files[1] - files[0])))
+    
+    sys.stdout.write("\n")
 
-    if files_bar_width > bar_width:
-        files_bar_width = bar_width
-
-    # print the files quota
-    format = "%8s: |\033[1;%dm%-35s\033[0m|\033[0;33m%3d\033[0m%%, \033[0;33m%4d\033[0mUsed,    \033[0;33m%4d\033[0mFree\n"
-    sys.stdout.write(format % ("  file count", colour(files_percent), "=" * int(files_bar_width), files_percent, files[0], (files[1] - files[0])))
-
-sys.stdout.write("\n")
-
-# vim: ft=python3
+if __name__ == '__main__':
+  run()
